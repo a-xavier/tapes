c63fde3878b058aa8b601caec416a557e61a4b92
a-xavier <43228621+a-xavier@users.noreply.github.com>
2019-04-03 22:27:32 +0000

    Changed Plotting labels to box

--- t_func.py
+++ t_func.py
@@ -2597,21 +2597,24 @@ def csv_report(full_stuff, output_path, suffix, ref_anno, tab_output):
 
     wot_wot = full_stuff['Func.{}'.format(ref_anno)].value_counts()
     plot_loc = wot_wot.plot(kind='pie', figsize=(15, 12), legend=False, fontsize=12, rot=15,
-                            title='Genomic Location of all variants')
+                            title='Genomic Location of all variants', labels = None)
+    plot_loc.legend(loc='best', bbox_to_anchor=(0, 0., 0.5, 0.5), labels=wot_wot.index)
     fig_loc = plot_loc.get_figure()
     fig_loc.savefig(os.path.join(output_path, (suffix + '_location')))
     fig_loc.clear()
 
     type_series = full_stuff['ExonicFunc.{}'.format(ref_anno)].value_counts()
     plot_type = type_series.plot(kind='pie', figsize=(15, 12), legend=False, fontsize=12, rot=15,
-                                 title='Type of variants')
+                                 title='Type of variants', labels = None)
+    plot_type.legend(loc='best', bbox_to_anchor=(0, 0., 0.5, 0.5), labels=type_series.index)
     fig_type = plot_type.get_figure()
     fig_type.savefig(os.path.join(output_path, (suffix + '_type')))
     fig_type.clear()
 
     pred_series = full_stuff['Prediction_ACMG_tapes'].value_counts()
     plot_pred = pred_series.plot(kind='pie', figsize=(15, 12), legend=False, fontsize=12, rot=15,
-                                 title='Tapes prediction')
+                                 title='Tapes prediction', labels = None)
+    plot_pred.legend(bbox_to_anchor=(0, 0., 0.5, 0.5),labels=pred_series.index)
     fig_pred = plot_pred.get_figure()
     fig_pred.savefig(os.path.join(output_path, (suffix + '_prediction')))
     fig_pred.clear()
