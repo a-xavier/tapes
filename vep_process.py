5e4ef96e93b688fd5c3e6503cb928dd3fcf1fa77
a-xavier <43228621+a-xavier@users.noreply.github.com>
2019-04-04 11:39:51 +1100

    Update vep_process.py

--- vep_process.py
+++ vep_process.py
@@ -1,3 +1,13 @@
+'''
+    Name: tapes.py
+    Author: Alexandre Xavier
+    Version; 0.1
+    Contact: alexandre.xavier@uon.edu.au
+    Python Version: 3.x
+    from https://github.com/a-xavier/tapes
+    Licence: MIT
+'''
+
 import pandas as pd
 import t_func as tf
 import json
